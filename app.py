20998d9e98132436cb88681e16f5ca6d33ad10c5
Mithilkumar Shashikant Patel <mithilpatel140@gmail.com>
2022-04-17 13:00:54 -0500

    my first commit

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask, Response, jsonify, render_template, logging, request, make_response
+'''
+Goal of LSTM microservice:
+1. LSTM microservice will accept the GitHub data from Flask microservice and will forecast the data for next 1 year based on past 30 days
+2. It will also plot three different graph (i.e.  "Model Loss", "LSTM Generated Data", "All Issues Data") using matplot lib 
+3. This graph will be stored as image in Google Cloud Storage.
+4. The image URL are then returned back to Flask microservice.
+'''
+# Import all the required packages
+from flask import Flask, jsonify, request, make_response
 import os
-import dateutil.relativedelta
 from dateutil import *
-from datetime import date
-from datetime import datetime, timedelta
+from datetime import timedelta
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import time
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 # Tensorflow (Keras & LSTM) related packages
 import tensorflow as tf
@@ -18,14 +24,17 @@ from tensorflow.python.keras.layers import Input, Dense, LSTM, Dropout
 from tensorflow.python.keras.callbacks import EarlyStopping
 from sklearn.preprocessing import MinMaxScaler
 
-# Google Cloud Storage
+# Import required storage package from Google Cloud Storage
 from google.cloud import storage
 
+# Initilize flask app
 app = Flask(__name__)
+# Handles CORS (cross-origin resource sharing)
 CORS(app)
-
+# Initlize Google cloud storage client
 client = storage.Client()
 
+# Add response headers to accept all types of  requests
 
 def build_preflight_response():
     response = make_response()
@@ -35,6 +44,7 @@ def build_preflight_response():
                          "PUT, GET, POST, DELETE, OPTIONS")
     return response
 
+#  Modify response headers when returning to the origin
 
 def build_actual_response(response):
     response.headers.set("Access-Control-Allow-Origin", "*")
@@ -42,6 +52,10 @@ def build_actual_response(response):
                          "PUT, GET, POST, DELETE, OPTIONS")
     return response
 
+'''
+API route path is  "/api/forecast"
+This API will accept only POST request
+'''
 
 @app.route('/api/forecast', methods=['POST'])
 def forecast():
@@ -65,25 +79,31 @@ def forecast():
     X = lzip(*days.index.values)[0]
     firstDay = min(X)
 
-    # To achieve data consistancy with both actual data and predicted values, I'm adding zeros to dates that do not have orders
-    # [firstDay + timedelta(days=day) for day in range((max(X) - firstDay).days + 1)]
+    '''
+    To achieve data consistancy with both actual data and predicted values, 
+    add zeros to dates that do not have orders
+    [firstDay + timedelta(days=day) for day in range((max(X) - firstDay).days + 1)]
+    '''
     Ys = [0, ]*((max(X) - firstDay).days + 1)
     days = pd.Series([firstDay + timedelta(days=i)
                       for i in range(len(Ys))])
     for x, y in zip(X, Y):
         Ys[(x - firstDay).days] = y
 
-    # modify the data that is suitable for LSTM
+    # Modify the data that is suitable for LSTM
     Ys = np.array(Ys)
     Ys = Ys.astype('float32')
     Ys = np.reshape(Ys, (-1, 1))
+    # Apply min max scaler to transform the data
     scaler = MinMaxScaler(feature_range=(0, 1))
     Ys = scaler.fit_transform(Ys)
+    # Divide training - test data with 80-20 split
     train_size = int(len(Ys) * 0.80)
     test_size = len(Ys) - train_size
     train, test = Ys[0:train_size, :], Ys[train_size:len(Ys), :]
     print('train size:', len(train), ", test size:", len(test))
 
+    # Create the training and test dataset
     def create_dataset(dataset, look_back=1):
         X, Y = [], []
         for i in range(len(dataset)-look_back-1):
@@ -91,34 +111,45 @@ def forecast():
             X.append(a)
             Y.append(dataset[i + look_back, 0])
         return np.array(X), np.array(Y)
-
-    # Look back decides how many days of data the model looks at for prediction
-    look_back = 1  # Here LSTM looks at approximately one month data
+    '''
+    Look back decides how many days of data the model looks at for prediction
+    Here LSTM looks at approximately one month data
+    '''
+    look_back = 1
     X_train, Y_train = create_dataset(train, look_back)
     X_test, Y_test = create_dataset(test, look_back)
 
-    # reshape input to be [samples, time steps, features]
+    # Reshape input to be [samples, time steps, features]
     X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
     X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
 
-    # verifying the shapes
+    # Verifying the shapes
     X_train.shape, X_test.shape, Y_train.shape, Y_test.shape
 
-    # # Model to forecast orders for all zip code
+    # Model to forecast
     model = Sequential()
     model.add(LSTM(100, input_shape=(X_train.shape[1], X_train.shape[2])))
     model.add(Dropout(0.2))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
 
+    # Fit the model with training data and set appropriate hyper parameters
     history = model.fit(X_train, Y_train, epochs=20, batch_size=70, validation_data=(X_test, Y_test),
                         callbacks=[EarlyStopping(monitor='val_loss', patience=10)], verbose=1, shuffle=False)
 
-    # create image Urls
+    '''
+    Creating image URL
+    BASE_IMAGE_PATH refers to Google Cloud Storage Bucket URL.Add your Base Image Path in line 145
+    if you want to run the application local
+    LOCAL_IMAGE_PATH refers local directory where the figures generated by matplotlib are stored
+    These locally stored images will then be uploaded to Google Cloud Storage
+    '''
     BASE_IMAGE_PATH = os.environ.get(
         'BASE_IMAGE_PATH', 'YOUR BASE_IMAGE_PATH')
+    # DO NOT DELETE "static/images" FOLDER as it is used to store figures/images generated by matplotlib
     LOCAL_IMAGE_PATH = "static/images/"
 
+    # Creating the image path for model loss, LSTM generated image and all issues data image
     MODEL_LOSS_IMAGE_NAME = "model_loss_" + type + ".png"
     MODEL_LOSS_URL = BASE_IMAGE_PATH + MODEL_LOSS_IMAGE_NAME
 
@@ -128,12 +159,13 @@ def forecast():
     ALL_ISSUES_DATA_IMAGE_NAME = "all_issues_data_" + type + ".png"
     ALL_ISSUES_DATA_URL = BASE_IMAGE_PATH + ALL_ISSUES_DATA_IMAGE_NAME
 
-    # Bucket Name
+    # Add your unique Bucket Name if you want to run it local
     BUCKET_NAME = os.environ.get(
         'BUCKET_NAME', 'Your BUCKET_NAME')
 
-    # model.summary()
+    # Model summary()
 
+    # Plot the model loss image
     plt.figure(figsize=(8, 4))
     plt.plot(history.history['loss'], label='Train Loss')
     plt.plot(history.history['val_loss'], label='Test Loss')
@@ -141,11 +173,13 @@ def forecast():
     plt.ylabel('Loss')
     plt.xlabel('Epochs')
     plt.legend(loc='upper right')
+    # Save the figure in /static/images folder
     plt.savefig(LOCAL_IMAGE_PATH + MODEL_LOSS_IMAGE_NAME)
 
-    # predict issues for test data
+    # Predict issues for test data
     y_pred = model.predict(X_test)
 
+    # Plot the LSTM Generated image
     fig, axs = plt.subplots(1, 1, figsize=(10, 4))
     X = mdates.date2num(days)
     axs.plot(np.arange(0, len(Y_train)), Y_train, 'g', label="history")
@@ -157,8 +191,10 @@ def forecast():
     axs.set_title('LSTM Generated Data For ' + type)
     axs.set_xlabel('Time Steps')
     axs.set_ylabel('Issues')
+    # Save the figure in /static/images folder
     plt.savefig(LOCAL_IMAGE_PATH + LSTM_GENERATED_IMAGE_NAME)
 
+    # Plot the All Issues data images
     fig, axs = plt.subplots(1, 1, figsize=(10, 4))
     X = mdates.date2num(days)
     axs.plot(X, Ys, 'purple', marker='.')
@@ -169,9 +205,10 @@ def forecast():
     axs.set_title('All Issues Data')
     axs.set_xlabel('Date')
     axs.set_ylabel('Issues')
+    # Save the figure in /static/images folder
     plt.savefig(LOCAL_IMAGE_PATH + ALL_ISSUES_DATA_IMAGE_NAME)
 
-    # Creates a new bucket and uploads an object
+    # Uploads an images into the google cloud storage bucket
     bucket = client.get_bucket(BUCKET_NAME)
     new_blob = bucket.blob(MODEL_LOSS_IMAGE_NAME)
     new_blob.upload_from_filename(
@@ -183,15 +220,16 @@ def forecast():
     new_blob.upload_from_filename(
         filename=LOCAL_IMAGE_PATH + LSTM_GENERATED_IMAGE_NAME)
 
+    # Construct the response
     json_response = {
         "model_loss_image_url": MODEL_LOSS_URL,
         "lstm_generated_image_url": LSTM_GENERATED_URL,
         "all_issues_data_image": ALL_ISSUES_DATA_URL
     }
-
+    # Returns image url back to flask microservice
     return jsonify(json_response)
 
 
-# run server
+# Run LSTM app server on port 8080
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
--- readme.txt
+++ readme.txt
@@ -12,20 +12,28 @@ Step2: What is Google Cloud Storage?
 
 Step3: Deploying LSTM to gcloud platform
        1: You must have Docker(https://www.docker.com/get-started) and Google Cloud SDK(https://cloud.google.com/sdk/docs/install) 
-           installed on your computer. Then, Create a gcloud project FOR LSTM  and enable the billing account. 
+           installed on your computer.  
        2. Steps to follow while creating LSTM gcloud project:
-            1. Go to GCP Platform, create a LSTM gcloud project.
-            2. After creating the LSTM gcloud project, go to gcloud storage of LSTM gcloud project then click on 
-               create bucket, then add name to your bucket click on continue, then choose where to store your data, there 
-               choose location type to "region" and location to "us-central1(Iowa)", then click on continue, then Choose a default storage class for your data
-               and choose option "standard" and click continue and then click on create then ypu will automatically navigated to "bucket details", there you 
-               will see "objects" and in "objects" you will see "buckets" click on "buckets", you will be able to see your assigned bucket name with checkbox
-               click on checkbox, then on the right side you will be able to see "permission" and "labels",in "permission" scroll down you will be able to see
-               "ADD PRINCIPAL" click on "ADD PRINCIPAL" you will be able to see "new principals" and "select a role", in "new principals" type "allUsers" and in 
-               "select a role" go to "cloud storage" click on "cloud storage" and select "storage object viewer" and hit on save.
+            1. Go to GCP Platform, create a LSTM gcloud project.enable the following:
+               a.billing account
+               b.Conatiner Registry API
+               c.Cloudbuild API
+            2. After creating the LSTM gcloud project follow the below steps:
+               a. go to gcloud storage of LSTM gcloud project then click on create bucket.
+               b. then add name to your bucket (bucket name should be unique) click on continue.
+               c. then choose where to store your data.
+               d. there choose location type to "region" and location to "us-central1(Iowa)", then click on continue.
+               e. then Choose a default storage class for your data
+               f. choose option "standard" and click continue and then click on create then you will automatically navigated to "bucket details"
+               g. there you will see "objects" and in "objects" you will see "buckets" click on "buckets", you will be able to see your assigned bucket name with checkbox
+               h. click on checkbox, then on the right side you will be able to see "permission" and "labels",in "permission" scroll down you will be able to see
+                  "ADD PRINCIPAL" 
+               i. click on "ADD PRINCIPAL" you will be able to see "new principals" and "select a role", in "new principals" type "allUsers" and in 
+                  "select a role" go to "cloud storage" click on "cloud storage" and select "storage object viewer" and hit on save after this
+               j.  copy the BUCKET_NAME (your unique assigned bucket name) and paste it in sticky notes for futher use.
             3. After this, go to "https://cloud.google.com/docs/authentication/getting-started#create-service-account-console"  and 
                there you will see creating service account, go to creating service account, click on console, and hit on creating service account
-            4. Then select your LSTM created project, in service account details add service name "lstm-github-forecasting" and click on "create and 
+            4. Then select your LSTM created project, in service account details add service name "your unique service name"(service name should be unique) and click on "create and 
                continue", then in Grant this service account access to project click on "select the role",choose "basic" and choose "owner"and hit on done.
             5. After that you will see your created service, click on the created service, go to keys, click on add key, and click create new key.
             6. Created key will get downloaded in .json format, copy that downloaded file in the given LSTM code
@@ -40,29 +48,30 @@ Step3: Deploying LSTM to gcloud platform
        6: Now type `docker tag <your newest image id> gcr.io/<your project-id>/<project-name>` and hit enter 
             Type `docker images` to see your image id updated with tag name
 
-       7: Type `gcloud init` on cmd
+       7: Type `gcloud init` on cmd and it will prompt Create or select a configuration choose existing configurations and hit enter and
+          it will prompt Choose a current Google Cloud project, choose your current gcloud project number and hit enter.
+          
 
-       8: Type `gcloud auth configure-docker` on cmd
+       8: Type `gcloud auth configure-docker` on cmd hit enter and then type "docker images"
 
-       9: Go to your GCloud account and open container registry
+       9: Type `docker push <your newest created tag>` on cmd and hit enter
 
-       10: Enable your billing account
-
-       11: Enable your Container Registry API
-
-       12: Go to the Cloud Build and enable Cloud Build API
-
-       13: Type `docker push <your newest created tag>` on cmd and hit enter
-
-       14: Go to cloud run and create new service, service name will be your GCloud project name and for container image url 
-            hit select and selects your latest id and hit select and edit container port to '8080', increase the memory limit 
-            to 1GiB and go to variable and secrets and click on add environment variable as follows(there will three environment variable):
+       10: Go to container registry you will see your newly pushed docker image, click on that docker image, after clicking, you will be able to see
+          docker image id and on the right side you will see "⋮", left click on "⋮" there you will be able to see option "deploy it to cloud run" click on that and 
+          it will navigate you to cloud run where in container image url hit select and select your latest id and change the Min Instance to 1 instead of 0 and the option 
+          to allow unauthorized access when creating new service and then go to container tab and edit container port to '8080', increase the memory limit 
+          to 1GiB and go to variable and secrets tab and click on add environment variable as follows(there will three environment variable):
                 Name                                 value  
-            a. GOOGLE_APPLICATION_CREDENTIALS     "<yourjsonfile>.json"
+            a. GOOGLE_APPLICATION_CREDENTIALS     "<your_json_filename>.json"
             b. BASE_IMAGE_PATH                    "https://storage.googleapis.com/your bucket name/"
             c. BUCKET_NAME                         "your bucket name"
+            
+      NOTE: For the environment variable, ".json" and "/" is mandatory in there respective environment variable value
+            because ".json" have all your gcloud information and secrets keys and "/" is a app route path.
 
-       16: Hit the create, this will create the service on port 8080 and will generate the url, hit the url.
+       11: Hit the create, this will create the service on port 8080 and will generate the url, hit the url.
+
+       12. Copy the generated LSTM gcloud url and paste it in sticky notes for further use
 
      
 Step4: To run locally:
