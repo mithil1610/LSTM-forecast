7d1bdb49dd49316fec0beb33d97b4faf32e6225f
Mithilkumar Shashikant Patel <mithilpatel140@gmail.com>
2022-04-21 12:28:17 -0500

    changes to files

--- app.py
+++ app.py
@@ -215,126 +215,46 @@ def forecast():
     axs.set_ylabel('Issues')
     # Save the figure in /static/images folder
     plt.savefig(LOCAL_IMAGE_PATH + ALL_ISSUES_DATA_IMAGE_NAME)
-
-    repo_names = [ "angular/angular",
-        "angular/material",
-        "angular/angular-cli",
-        "SebastianM/angular-google-maps",
-        "d3/d3",
-        "facebook/react",
-        "tensorflow/tensorflow",
-        "keras-team/keras",
-        "pallets/flask" ]
-    GITHUB_URL = f"https://api.github.com/"
-    token = os.environ.get(
-        'GITHUB_TOKEN', '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')
-    headers = {
-        "Authorization": f'token {token}'
-    }
-    params = {
-        "state": "open"
-    }
-    created_sum = []
-    closed_sum = []
-    for j in range(len(repo_names)):
-        issues_reponse = []
-        today = date.today()
-        for i in range(24):
-            last_month = today + dateutil.relativedelta.relativedelta(months=-1)
-            types = 'type:issue'
-            repo = 'repo:' + repo_names[j]
-            ranges = 'created:' + str(last_month) + '..' + str(today)
-            per_page = 'per_page=100'
-            # Search query will create a query to fetch data for a given repository in a given time range
-            search_query = types + ' ' + repo + ' ' + ranges
-
-            # Append the search query to the GitHub API URL 
-            query_url = GITHUB_URL + "search/issues?q=" + search_query + "&" + per_page
-            # requsets.get will fetch requested query_url from the GitHub API
-            search_issues = requests.get(query_url, headers=headers, params=params)
-            # Convert the data obtained from GitHub API to JSON format
-            search_issues = search_issues.json()
-            issues_items = []
-            try:
-                # Extract "items" from search issues
-                issues_items = search_issues.get("items")
-            except KeyError:
-                error = {"error": "Data Not Available"}
-                resp = Response(json.dumps(error), mimetype='application/json')
-                resp.status_code = 500
-                print(resp)
-            if issues_items is None:
-                continue
-            for issue in issues_items:
-                label_name = []
-                data = {}
-                current_issue = issue
-                # Get issue number
-                data['issue_number'] = current_issue["number"]
-                # Get created date of issue
-                data['created_at'] = current_issue["created_at"][0:10]
-                if current_issue["closed_at"] == None:
-                    data['closed_at'] = current_issue["closed_at"]
-                else:
-                    # Get closed date of issue
-                    data['closed_at'] = current_issue["closed_at"][0:10]
-                for label in current_issue["labels"]:
-                    # Get label name of issue
-                    label_name.append(label["name"])
-                data['labels'] = label_name
-                # It gives state of issue like closed or open
-                data['State'] = current_issue["state"]
-                # Get Author of issue
-                data['Author'] = current_issue["user"]["login"]
-                issues_reponse.append(data)
-
-            today = last_month
-
-        df = pd.DataFrame(issues_reponse)
-        
-        if df.empty:
-            created_sum.append([repo_names[j], 0])
-            closed_sum.append([repo_names[j], 0])
-        else:
-            created_at = []
-            if not df['created_at'].empty:
-                created_at = df['created_at']
-            month_issue_created = pd.to_datetime(
-                pd.Series(created_at), format='%Y/%m/%d')
-            month_issue_created.index = month_issue_created.dt.to_period('m')
-            month_issue_created = month_issue_created.groupby(level=0).size()
-            month_issue_created = month_issue_created.reindex(pd.period_range(
-                month_issue_created.index.min(), month_issue_created.index.max(), freq='m'), fill_value=0)
-            month_issue_created_dict = month_issue_created.to_dict()
-            sum_created = 0
-            for key in month_issue_created_dict.keys():
-                sum_created += month_issue_created_dict[key]
-            created_sum.append([repo_names[j], sum_created])
-
-            closed_at = []
-            if not df['closed_at'].empty:
-                closed_at = df['closed_at'].sort_values(ascending=True)
-            month_issue_closed = pd.to_datetime(
-                pd.Series(closed_at), format='%Y/%m/%d')
-            month_issue_closed.index = month_issue_closed.dt.to_period('m')
-            month_issue_closed = month_issue_closed.groupby(level=0).size()
-            month_issue_closed = month_issue_closed.reindex(pd.period_range(
-                month_issue_closed.index.min(), month_issue_closed.index.max(), freq='m'), fill_value=0)
-            month_issue_closed_dict = month_issue_closed.to_dict()
-            sum_closed = 0        
-            for key in month_issue_closed_dict.keys():
-                sum_closed += month_issue_closed_dict[key]
-            closed_sum.append([repo_names[j], sum_closed])
     
-    plt.figure(figsize=(8, 4))
+    
+    created_at_issues = []
+    closed_at_issues = []
+    if not df.empty:
+        created_at = df['created_at']
+        month_issue_created = pd.to_datetime(
+            pd.Series(created_at), format='%Y/%m/%d')
+        month_issue_created.index = month_issue_created.dt.to_period('m')
+        month_issue_created = month_issue_created.groupby(level=0).size()
+        month_issue_created = month_issue_created.reindex(pd.period_range(
+            month_issue_created.index.min(), month_issue_created.index.max(), freq='m'), fill_value=0)
+        month_issue_created_dict = month_issue_created.to_dict()
+        for key in month_issue_created_dict.keys():
+            array = [str(key), month_issue_created_dict[key]]
+            created_at_issues.append(array)
+
+        closed_at = df['closed_at'].sort_values(ascending=True)
+        month_issue_closed = pd.to_datetime(
+            pd.Series(closed_at), format='%Y/%m/%d')
+        month_issue_closed.index = month_issue_closed.dt.to_period('m')
+        month_issue_closed = month_issue_closed.groupby(level=0).size()
+        month_issue_closed = month_issue_closed.reindex(pd.period_range(
+            month_issue_closed.index.min(), month_issue_closed.index.max(), freq='m'), fill_value=0)
+        month_issue_closed_dict = month_issue_closed.to_dict()
+        for key in month_issue_closed_dict.keys():
+            array = [str(key), month_issue_closed_dict[key]]
+            closed_at_issues.append(array)
+    
+    plt.figure(figsize=(12, 6))
+    x = []
     arr_y1 = []
-    for i in range(len(created_sum)):
-        arr_y1.append(created_sum[i][1])
+    for i in range(len(created_at_issues)):
+        x.append(created_at_issues[i][0])
+        arr_y1.append(created_at_issues[i][1])
     arr_y2 = []
-    for i in range(len(closed_sum)):
-        arr_y2.append(closed_sum[i][1])
-    plt.bar(repo_names, arr_y1, color = 'blue')
-    plt.bar(repo_names, arr_y2, bottom = arr_y1, color='yellow')
+    for i in range(len(closed_at_issues)):
+        arr_y2.append(closed_at_issues[i][1])
+    plt.bar(x, arr_y1, color = 'blue')
+    plt.bar(x, arr_y2, bottom = arr_y1, color='yellow')
     plt.legend(["created_at", "closed_at"])
     plt.xticks(rotation=90)
     plt.title('Stacked bar chart for to plot the created and closed issues for every Repository')
